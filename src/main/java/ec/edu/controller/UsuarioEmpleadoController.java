1b476aae67231f6397116ba8281bf9d9676db45a
DennisJav <denis.javier.tapia.ortiz@gmail.com>
2022-04-04 22:40:31 -0500

    ingresos

--- src/main/java/ec/edu/controller/UsuarioEmpleadoController.java
+++ src/main/java/ec/edu/controller/UsuarioEmpleadoController.java
@@ -1,16 +1,12 @@
 package ec.edu.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import ec.edu.modelo.Usuario;
 import ec.edu.modelo.Vehiculo;
@@ -18,40 +14,46 @@ import ec.edu.service.IUsuarioService;
 import ec.edu.service.IVehiculoService;
 
 @Controller
-@RequestMapping("/usuarios/")
-public class UsuarioController {
+@RequestMapping("/empleados/")
+public class UsuarioEmpleadoController {
 
 	@Autowired
 	private IUsuarioService usuarioService;
 	@Autowired
 	private IVehiculoService vehiculoService;
-
-	@GetMapping("usuarioN")
-	public String vistaPaginaIngresoDatos(Usuario usuario) {
-		return "usuarioNuevo";
+	
+	
+	@GetMapping("registrarC")
+	public String vistaPaginaIngresoDatosCliente(Usuario usuario) {
+		return "registrarCliente";
 	}
-
+	
 	@PostMapping("insertar")
 	public String insertarUsuarioCliente(Usuario usuario, BindingResult result, Model modelo) {
 		this.usuarioService.insertarUsuario(usuario);
-		return "usuarioRegistradoNotify";
+		return "clienteRegistradoNotify";
 	}
-
-	@GetMapping("datosVehiculos")
-	public String mostrarMarcaModelo(Vehiculo vehiculo) {
-		return "mostrarMarcaModelo";
+	
+	
+	@GetMapping("datosCliente")
+	public String vistaPaginaBuscarCliente(Usuario usuario) {
+		return "buscarCliente";
 	}
-
-	@GetMapping("buscar/disponibles")
-	public String buscarVehiculosMarcaModelo(Vehiculo vehiculo, Model modelo) {
-
-		List<Vehiculo> vehiculosDisponibles = this.vehiculoService.buscarMarcaModelo(vehiculo.getMarca(),
-				vehiculo.getModelo());
-		modelo.addAttribute("vehiculosDisponibles",vehiculosDisponibles);
-		return "listaVehiculosDisponibles";
+	
+	@GetMapping("buscarCliente")
+	public String buscarClienteCedula(Usuario usuario, Model modelo) {
+		Usuario clienteBuscado = this.usuarioService.buscarUsuarioCedula(usuario.getCedula());
+		modelo.addAttribute("clienteBuscado",clienteBuscado);
+		return "mostrarCliente";
+	}
+	
+	
+	@GetMapping("datosVehiculo")
+	private String vistaPaginaInsertarDatosVehiculo(Vehiculo vehiculo) {
+		return "registrarVehiculo";
 	}
 	
 	
 	
-
+	
 }
